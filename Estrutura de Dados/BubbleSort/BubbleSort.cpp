ddf7abc6aa75dea42cc81baf7b919f541ede2d9f
lenny <israelalves45155@gmail.com>
2021-12-26 17:09:29 -0300

    v2

--- Estrutura de Dados/BubbleSort/BubbleSort.cpp
+++ Estrutura de Dados/BubbleSort/BubbleSort.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
-#include <new>
-#include <string>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string>
 
-int main()
+#define TAM 10
+
+using namespace std;
+
+void imprime(int vetor[])
 {
+    int i;
+
+    for (i = 0; i < TAM; i++)
+    {
+        cout << "|" << vetor[i] << "|";
+    }
+}
+int main(int argc, char const *argv[])
+{
+    int vetor[TAM] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int x, y, aux;
+
+    for (x = 0; x < TAM; x++)
+    {
+        for (y = 0; y < TAM; y++)
+        {
+            if (vetor[x] > vetor[y])
+            {
+                aux = vetor[x];
+                vetor[x] = vetor[y];
+                vetor[y] = aux;
+            }
+        }
+    }
+    imprime(vetor);
 
     return 0;
 }
